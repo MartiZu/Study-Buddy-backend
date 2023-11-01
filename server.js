3dcca81da886d48678b86e42f78079dc7bdad92c
MartiZu <martina.zurli@gmail.com>
2023-11-01 19:32:36 +0000

    backend separated

---
+++ server.js
@@ -0,0 +1,8 @@
+import { app } from "./app.js";
+
+const PORT = process.env.PORT ?? 7000;
+const HOST = process.env.HOST ?? "0.0.0.0.";
+
+app.listen(PORT,HOST, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
